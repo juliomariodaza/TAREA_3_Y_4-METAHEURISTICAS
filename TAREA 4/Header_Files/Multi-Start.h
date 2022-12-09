0a91dfb29acd1b95d217557c95e3f8296a59498d
Julio Mario Daza Escorcia <juliomariodaza@gmail.com>
2022-12-09 11:49:41 -0500

    Add files via upload

---
+++ TAREA 4/Header_Files/Multi-Start.h
@@ -0,0 +1,32 @@
+#pragma once
+
+
+struct solution {
+	int* tour;			//This is what the fitness_evaluation function in EVRP.hpp will evaluate
+	int id;
+	double tour_length; //Quality of the solution
+	int steps;			//Size of the solution
+	int* EVs;			//steps for route
+	//the format of the solution is as follows:
+	//*tour:  0 - 5 - 6 - 8 - 0 - 1 - 2 - 3 - 4 - 0 - 7 - 0
+	//*steps: 12
+	//this solution consists of three routes: 
+	//Route 1: 0 - 5 - 6 - 8 - 0
+	//Route 2: 0 - 1 - 2 - 3 - 4 - 0
+	//Route 3: 0 - 7 - 0
+	// EVs[0]: count of route
+	// EVs[1]: steps to finalize the route 1
+	// EVs[2]: steps to finalize the route 2
+	// EVs[3]: steps to finalize the route 3
+};
+
+
+extern solution* best_sol;
+
+
+void initialize_Multi_Start();
+void run_Multi_Start();
+
+void run_Local_Search();
+
+void free_Multi_Start();
---
+++ TAREA 4/Source_Files/Multi-Start.cpp
@@ -0,0 +1,204 @@
+#include<cmath>
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string>
+#include<cstring>
+#include<math.h>
+#include<fstream>
+#include<limits.h>
+
+#include "../Header_Files/Multi-Start.h"
+#include "../Header_Files/EVRP.h"
+
+using namespace std;
+
+
+solution* best_sol;   //see heuristic.hpp for the solution structure
+
+/*initialize the structure of your heuristic in this function*/
+void initialize_Multi_Start()
+{
+    best_sol = new solution;
+    best_sol->tour = new int[NUM_OF_CUSTOMERS + 1000];
+    best_sol->id = 1;
+    best_sol->steps = 0;
+    best_sol->tour_length = INT_MAX;
+    best_sol->EVs = new int[MIN_VEHICLES * 2];
+}
+
+
+/*implement your heuristic in this function*/
+void run_Multi_Start() {
+
+    /*Generate a random solution for the random heuristic*/
+    int i, help, object, tot_assigned = 0;
+    int* r;
+    double energy_temp = 0.0;
+    double capacity_temp = 0.0;
+    int from, to, trucks = 0;
+    int charging_station;
+    double temp_double;
+
+    r = new int[NUM_OF_CUSTOMERS + 1];
+    
+    //Set indexes of objects
+    for (i = 1; i <= NUM_OF_CUSTOMERS; i++)
+    {
+        r[i - 1] = i;
+    }
+      
+     //Randomly change indexes of objects
+    for (i = 0; i <= NUM_OF_CUSTOMERS; i++) 
+    {
+        object = (int)((rand() / (RAND_MAX + 1.0)) * (double)(NUM_OF_CUSTOMERS - tot_assigned));
+        help = r[i];
+        r[i] = r[i + object];
+        r[i + object] = help;
+        tot_assigned++;
+    }
+
+    // Initialize the route
+    best_sol->steps = 0;
+    best_sol->tour_length = INT_MAX;
+    best_sol->tour[0] = DEPOT;
+    best_sol->steps++;
+    best_sol->EVs[0] = 1;
+
+    i = 0;
+    while (i < NUM_OF_CUSTOMERS) 
+    {
+        from = best_sol->tour[best_sol->steps - 1];
+        to = r[i];
+
+        //double DEMANDA = (capacity_temp + get_customer_demand(to));
+        //double BATERIA = (energy_temp + get_energy_consumption(from, to));
+        
+        if ((capacity_temp + get_customer_demand(to)) <= MAX_CAPACITY && (energy_temp + get_energy_consumption(from, to)) <= 0.8*BATTERY_CAPACITY) 
+        {// EV have CAPACITY and BATTERY
+            capacity_temp += get_customer_demand(to);
+            energy_temp += get_energy_consumption(from, to);
+            best_sol->tour[best_sol->steps] = to;
+            best_sol->steps++;
+            i++;
+        }
+        else if ((capacity_temp + get_customer_demand(to)) > MAX_CAPACITY) 
+        {// EV not have CAPACITY
+            capacity_temp = 0.0;
+            energy_temp = 0.0;
+            best_sol->tour[best_sol->steps] = DEPOT;
+            best_sol->steps++;
+            trucks++;
+            best_sol->EVs[trucks] = best_sol->steps;
+        }
+        else if (energy_temp + get_energy_consumption(from, to) > 0.8*BATTERY_CAPACITY) 
+        {// EV not have BATTERY
+            temp_double = (double)BATTERY_CAPACITY; 
+            charging_station = rand() % (ACTUAL_PROBLEM_SIZE - NUM_OF_CUSTOMERS - 1) + NUM_OF_CUSTOMERS + 1;
+            for (int ii = (NUM_OF_CUSTOMERS + 2); ii <= ACTUAL_PROBLEM_SIZE; ii++)
+            {
+                if (get_energy_consumption(from, to) < temp_double && (energy_temp + get_energy_consumption(from, to)) <= BATTERY_CAPACITY)
+                {
+                    temp_double = get_energy_consumption(from, to);
+                    charging_station = ii;
+                }
+            }            
+            if (is_charging_station(charging_station) == true) 
+            {
+                energy_temp = 0.0;
+                best_sol->tour[best_sol->steps] = charging_station;
+                best_sol->steps++;
+            }
+        }else 
+        {
+            if (best_sol->steps > 1)
+            {
+                capacity_temp = 0.0;
+                energy_temp = 0.0;
+                best_sol->tour[best_sol->steps] = DEPOT;
+                best_sol->steps++;
+                trucks++;
+                best_sol->EVs[trucks] = best_sol->steps;
+            }
+
+        }//End-if ((capacity_temp + get_customer_demand(to)) <= MAX_CAPACITY && (energy_temp + get_energy_consumption(from, to)) <= 0.8*BATTERY_CAPACITY)
+    }//End-while (i < NUM_OF_CUSTOMERS)
+
+    //Close EVRP tour to return back to the depot
+    if (best_sol->tour[best_sol->steps - 1] != DEPOT)
+    {
+        best_sol->tour[best_sol->steps] = DEPOT;
+        best_sol->steps++;
+        trucks++;
+        best_sol->EVs[trucks] = best_sol->steps;
+    }
+
+    best_sol->EVs[0] = trucks;
+
+    best_sol->tour_length = fitness_evaluation(best_sol->tour, best_sol->steps);
+
+
+    //free memory
+    delete[] r;
+
+    //print_solution(best_sol->tour, best_sol->steps);
+
+    //run_Local_Search();
+
+}
+
+
+/*implement your heuristic in this function*/
+void run_Local_Search() {
+
+    int i, nodei, j, nodej, posi=0, posj=0;
+    double before, after, improvement=1000000.0;
+
+    for (i = 1; i <= best_sol->steps - 2; i++)
+    {
+            nodei = best_sol->tour[i];                
+        if (nodei == DEPOT || is_charging_station(nodei) == true) continue;
+        for (j = i + 1; j <= best_sol->steps - 1; j++)
+        {
+                nodej = best_sol->tour[j];
+            if (nodej == DEPOT || i == j || is_charging_station(nodej) == true) continue;
+            before = get_distance(best_sol->tour[i - 1], nodei) +
+                    get_distance(nodei, nodej) +
+                    get_distance(nodej, best_sol->tour[j + 1]);
+            after = get_distance(best_sol->tour[i - 1], nodej) +
+                    get_distance(nodej, nodei) +
+                    get_distance(nodei, best_sol->tour[j + 1]);
+            if (after < before && after < improvement )
+            {
+                posi = i;
+                posj = j; 
+                improvement = after;
+                //cout << "\n Position i: " << posi << "\n Node i: " << nodei << endl;
+                //cout << "\n Position j: " << posj << "\n Node j: " << nodej << endl;
+            }// if (after < before)
+        }// for (j = i + 1; j <= best_sol->steps - 1; j++)
+    }// for (i = 1; i <= best_sol->steps - 2; i++)
+    
+    if ((posi + posj) > 0)
+    {
+        int 
+            node1 = best_sol->tour[posi],
+            node2 = best_sol->tour[posj];
+        
+        best_sol->tour[posi] = node2;
+        best_sol->tour[posj] = node1;
+        best_sol->tour_length = fitness_evaluation(best_sol->tour, best_sol->steps);
+        //print_solution(best_sol->tour, best_sol->steps);
+    }// if ((posi + posj) > 0)
+
+}
+
+
+
+/*free memory structures*/
+void free_Multi_Start()
+{
+    delete[] best_sol->tour;
+    delete[] best_sol->EVs;
+}
+
